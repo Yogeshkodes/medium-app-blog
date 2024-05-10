198b35412e1ab579cc14a35259c79e29330d4f67
Yogeshkodes <yogesh.cr712@gmail.com>
2024-05-10 12:40:18 +0530

    adding jwt and signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,37 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    JWT_SECRET: string;
+    DATABASE_URL: string;
+  };
+}>();
 
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.post("/api/v1/signup", (c) => {
-  return c.text("hellp world");
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+  return c.json({ jwt: token });
 });
 
 app.post("/api/v1/signin", (c) => {
   return c.text("hellp world");
 });
-
 
 app.post("/api/v1/blog", (c) => {
   return c.text("hellp world");
