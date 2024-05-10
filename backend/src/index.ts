bbc2b3ce8bf53b6801492bc45fac75113e3894a6
Yogeshkodes <yogesh.cr712@gmail.com>
2024-05-10 14:43:18 +0530

    made middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
@@ -9,6 +9,19 @@ const app = new Hono<{
     DATABASE_URL: string;
   };
 }>();
+
+app.use("/api/v1/blog/*", async (c, next) => {
+  const header = c.req.header("authorization") || "";
+  const token = header.split(" ")[1];
+
+  const response = await verify(token, c.env.JWT_SECRET);
+  if (response.id) {
+    next();
+  } else {
+    c.status(403);
+    return c.json({ error: "unauthorised" });
+  }
+});
 
 app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient({
@@ -29,8 +42,28 @@ app.post("/api/v1/signup", async (c) => {
   return c.json({ jwt: token });
 });
 
-app.post("/api/v1/signin", (c) => {
-  return c.text("hellp world");
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "User Not Found" });
+  }
+
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+  return c.json({ jwt });
 });
 
 app.post("/api/v1/blog", (c) => {
